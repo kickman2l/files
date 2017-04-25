57fda0649958ee112a394742cf4f285e3bfe3570
kickman2l <geraskopavel@gmail.com>
2017-04-25 21:36:25 +0300

    Update reg.py

--- reg.py
+++ reg.py
@@ -33,8 +33,7 @@ def is_groupe_exists():
             "output": "extend",
             "filter": {
                 "name": [
-                    "Zabbix servers",
-                    "Linux servers"
+                    "CloudHosts",
                 ]
             }
         },

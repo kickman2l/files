583c6f3a23622b851a474b007c8cb52adb539079
kickman2l <geraskopavel@gmail.com>
2017-04-25 21:32:56 +0300

    Update reg.py

--- reg.py
+++ reg.py
@@ -27,14 +27,14 @@ auth_token = post({
 
 def is_groupe_exists():
     results = post({
-    "jsonrpc": "2.0",
-    "method": "hostgroup.exists",
-    "params": {
-        "name": "CloudHosts"
-    },
-    "auth": auth_token,
-    "id": 1
-    }).json()["result"]
+        "jsonrpc": "2.0",
+        "method": "hostgroup.exists",
+        "params": {
+            "name": "CloudHosts"
+        },
+        "auth": auth_token,
+        "id": 1
+    }).json()
     return results
 
 def create_group(groupe_name):

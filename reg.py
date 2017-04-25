e91ad79e903bdd3732515d8a0e8cd0a8e6566f46
kickman2l <geraskopavel@gmail.com>
2017-04-25 19:37:39 +0300

    VG FILE AND JMX GATEWAY CONFIG

---
+++ reg.py
@@ -0,0 +1,52 @@
+import os, requests, json, sys
+from requests.auth import HTTPBasicAuth
+
+zabbix_server = "192.168.33.100"
+zabbix_api_admin_name = "Admin"
+zabbix_api_admin_password = "zabbix"
+
+def post(request):
+    headers = {'content-type': 'application/json'}
+    return requests.post(
+        "http://" + zabbix_server + "/api_jsonrpc.php",
+        data=json.dumps(request),
+        headers=headers,
+        auth=HTTPBasicAuth(zabbix_api_admin_name, zabbix_api_admin_password)
+    )
+
+auth_token = post({
+    "jsonrpc": "2.0",
+    "method": "user.authenticate",
+    "params": {
+        "user": zabbix_api_admin_name,
+        "password": zabbix_api_admin_password
+    },
+    "auth": None,
+    "id": 0}
+).json()["result"]
+
+def register_host(hostname, ip):
+    post({
+        "jsonrpc": "2.0",
+        "method": "host.create",
+        "params": {
+            "host": hostname,
+            "templates": [{
+                "templateid": "10001"
+            }],
+            "interfaces": [{
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": ip,
+                "dns": "",
+                "port": "10050"
+            }],
+            "groups": [
+                {"groupid": "1"},
+                {"groupid": "2"}
+            ]
+        },
+        "auth": auth_token,
+        "id": 1
+    })
